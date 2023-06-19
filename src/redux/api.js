0c812d5005dd243a4a5907cf353aedeb2f7ab0c4
Andriy Shepeha <andrii.shepeha.kn.2021@lpnu.ua>
2023-06-19 22:59:30 +0300

    backend translation

--- src/redux/api.js
+++ src/redux/api.js
@@ -6,6 +6,7 @@ const api = axios.create({
 
 api.interceptors.request.use((config) => {
   config.headers.Authorization = window.localStorage.getItem('profile')?.token
+  config.headers.lang = window.localStorage.getItem('i18nextLng') || 'en'
 
   return config
 })
